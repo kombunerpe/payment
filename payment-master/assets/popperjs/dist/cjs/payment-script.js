c56ff0a10f8035d5cb6b8bb82d34f800ecb33c31
kombunerpe <kombunicl@gmail.com>
2024-12-04 16:37:57 -0800

    Update payment-script.js

--- payment-master/assets/popperjs/dist/cjs/payment-script.js
+++ payment-master/assets/popperjs/dist/cjs/payment-script.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxGXnZcb8U-YirfJiv0RvkZPqSkbCQtp_6F8tBxEpU_7U-HtEiN91uDmjehHoE_AeM/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz4eE65LI4KV6b6iRsd5xHLWMHuudoQShAjdE6FeYwX_G2Lf0HRaZ3L2JpYHlTmOV4/exec';
 const form = document.forms['submit-to-google-sheet'];
 
 // Mengecek apakah scriptURL tidak kosong atau valid
